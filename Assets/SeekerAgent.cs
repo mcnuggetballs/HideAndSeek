4459433c3ecb71839aa164a4b1a4ff33b1f9485e
JTZQ <133725661+mcnuggetballs@users.noreply.github.com>
2026-05-11 16:33:12 +0800

    use RigidBody instead

--- Assets/SeekerAgent.cs
+++ Assets/SeekerAgent.cs
@@ -5,13 +5,16 @@ using Unity.MLAgents.Sensors; // for collect observation
 using Unity.MLAgents.Actuators;
 using UnityEngine.AI;
 
+
+
 // think of it like how to reward my agent into doing patrol/chase
 public class SeekerAgent : Agent
 {
-    public NavMeshAgent agent;
+    //public NavMeshAgent agent;
+    public Rigidbody rb;
     public float moveSpeed;
     public float rotateSpeed;
-    private float prevDistance=0;
+    private float prevDistance = 0;
 
     //public Rigidbody rb;
 
@@ -34,39 +37,62 @@ public class SeekerAgent : Agent
         pos1.y = 0.929f;
         target.position = pos1;
 
-        float prevDistance = (target.position - transform.position).magnitude; // to encourage agent to keep moving closer to target
+        prevDistance = (target.position - transform.position).magnitude; // to encourage agent to keep moving closer to target
     }
 
     // just putting information in his brain
     public override void CollectObservations(VectorSensor sensor)
     {
-        Vector3 dir = target.position - transform.position;
-        sensor.AddObservation(dir.normalized); // telling the agent to add into his brain
-        sensor.AddObservation(dir.magnitude); 
+        // know where is target
+        Vector3 dir = target.position - transform.position; // direction from seeker to target
+        sensor.AddObservation(dir.normalized); 
+        sensor.AddObservation(dir.magnitude);
+
+        // need to know if walking/turning
+        sensor.AddObservation(transform.forward);
+        sensor.AddObservation(rb.linearVelocity);
     }
 
     // make decision
     public override void OnActionReceived(ActionBuffers actions)
     {
-        // i want it to move and rotate
+
+        // either rotate
         float rotate = actions.ContinuousActions[0]; // telling him he can control this
-        transform.Rotate(Vector3.up,rotateSpeed*rotate*Time.deltaTime);
+        Quaternion delta = Quaternion.Euler(0,rotate * rotateSpeed * Time.deltaTime,0);
+        rb.MoveRotation(rb.rotation * delta);
 
+        // or rotate      
         float move = actions.ContinuousActions[1];
-        transform.Translate(transform.forward * moveSpeed * move * Time.deltaTime);
+        rb.MovePosition(rb.position + transform.forward * moveSpeed * move *Time.deltaTime); // move forward in current direction
 
+        // keep track of movement
         float currentdistance = Vector3.Distance(transform.position, target.position);
-
         float distanceDiff = prevDistance - currentdistance;
 
-        // rewards
+        // small reward if moved closer, penalise if further
         AddReward(distanceDiff * 0.1f);
         prevDistance = currentdistance;
 
-        if (currentdistance < 1.5f) 
+        // small penalty if wasting time every step to encourage faster decisions
+        AddReward(-0.001f);
+
+        // if not better than previous attempt penalty
+        if(distanceDiff < 0)
         {
+            AddReward(distanceDiff * 0.05f);
+        }
+    }
+
+    // what happens after collision
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Hider"))
+        {
+            // large reward
             AddReward(1.0f);
             EndEpisode();
         }
+
     }
 }
